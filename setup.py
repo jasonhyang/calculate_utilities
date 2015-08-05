80f06180f74c592e4cb81e582191928dfb023ced
dmccloskey <dmccloskey87@gmail.com>
2015-08-05 09:12:24 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="calculate_utilities",
+    version="1.0.b1",
+    description="Library of calculation methods using numpy, scipy, R, Matlab, and other open-source resources",
+    author="Douglas McCloskey",
+    author_email="dmccloskey87@gmail.com",
+    url="https://github.com/dmccloskey/calculate_utilities",
+    packages=["calculate_utilities"],
+    #classifiers=[
+    #'Development Status :: 5 - Production/Stable',
+    #'Environment :: Console',
+    #'Intended Audience :: Science/Research',
+    #'Operating System :: OS Independent',
+    #'Programming Language :: Python :: 3.4',
+    #'Programming Language :: Cython',
+    #'Programming Language :: Python :: Implementation :: CPython',
+    #'Topic :: Scientific/Engineering',
+    #'Topic :: Scientific/Engineering :: Bio-Informatics'
+    #],
+    platforms="GNU/Linux, Mac OS X >= 10.7, Microsoft Windows >= 7",
+    )
